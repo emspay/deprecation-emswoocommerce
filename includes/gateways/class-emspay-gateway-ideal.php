ce5126d90e513c5552a072e24f4fa09f768c3cb5
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2018-01-19 11:49:08 +0200

    Created redirect to EMS without receipt page

--- includes/gateways/class-emspay-gateway-ideal.php
+++ includes/gateways/class-emspay-gateway-ideal.php
@@ -162,4 +162,69 @@ class Emspay_Gateway_Ideal extends Emspay_Gateway {
 		return in_array( $currency, $this->supported_currencies );
 	}
 
+
+	/**
+	 * Process standard payments.
+	 *
+	 * @param WC_Order $order
+	 * @return array
+	 */
+	protected function process_hosted_payment($order)
+	{
+		$query_args = build_query( array( 'order_id' => $order->get_id() ) );
+
+		return array(
+			'order_id' => $order->get_id(),
+			'result' => 'success',
+			'redirect' => wc_get_checkout_url() . '?' . $query_args
+		);
+	}
+
+
+	/**
+	 * Init hook.
+	 */
+	protected function init_hook()
+	{
+		parent::init_hook();
+		add_filter( 'woocommerce_after_checkout_form', array( $this, 'redirect_to_ems' ));
+	}
+
+
+	/**
+	 * Redirect to EMS.
+	 */
+	public function redirect_to_ems() {
+
+		if ( ! empty( $_GET['order_id'] ) ) {
+			$order = wc_get_order( $_GET['order_id'] );
+
+			$args = $this->get_hosted_payment_args( $order );
+			foreach ( $args as $field => $value ) {
+				$this->core_order->{$field} = $value;
+			}
+
+			// Initialize payment
+			$hosted_payment = new EmsCore\Request( $this->core_order, $this->core_options );
+			$form_fields    = $hosted_payment->getFormFields();
+
+			self::log( 'Payment form fields for Order #' . $_GET['order_id'] . ' ' . print_r( $form_fields, TRUE ) );
+			?>
+			<form id="payForm" method="post"
+				action="<?php echo $hosted_payment->getFormAction(); ?>">
+				<?php foreach ( $form_fields as $name => $value ) { ?>
+					<input type="hidden" name="<?php echo $name; ?>"
+						value="<?php echo esc_attr( $value ); ?>">
+				<?php } ?>
+			</form>
+			<script>
+              jQuery(document).ready(function () {
+                jQuery('#payForm').submit();
+              });
+			</script>
+			<?php
+		}
+
+	}
+
 }
