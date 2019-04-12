1319911a3bd208d3fe618723d9aa6024aee94d2f
Dmitriy Mikolenko <dmitry.mikolenko@trive.amsterdam>
2019-04-12 18:04:38 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="emspay" # your svn username
+SVNUSER="dave.ligthart" # your svn username
 
 # Let's begin...
 echo ".........................................."
